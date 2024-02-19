ae2a24842c6ccfc15845afe43106c0b621befdb5
Pamela <pamelaurra2030@gmail.com>
2024-02-18 23:43:31 -0300

    viajes vs2

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+let git = $("#github")
+let face = $("#facebook")
+let link = $("#linkedin")
+let twit = $("#twitter")
+
+git.click(function () {
+
+    $(location).attr('href', 'https://github.com/')
+
+})
+
+face.click(function () {
+
+    $(location).attr('href', 'https://facebook.com/')
+
+})
+
+link.click(function () {
+
+    $(location).attr('href', 'https://linkedin.com/')
+
+})
+
+twit.click(function () {
+
+    $(location).attr('href', 'https://twitter.com/')
+
+})
+
+
+
+
+
+
