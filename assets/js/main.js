9fcb559e2f2a0126ed451af1ef02b3be784aeec3
Pamela <pamelaurra2030@gmail.com>
2024-02-20 23:58:24 -0300

    vs3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,37 @@
-let git = $("#github")
-let face = $("#facebook")
-let link = $("#linkedin")
-let twit = $("#twitter")
 
-git.click(function () {
+$(document).ready(function () {
 
-    $(location).attr('href', 'https://github.com/')
+    let git = $("#github")
+    let face = $("#facebook")
+    let link = $("#linkedin")
+    let twit = $("#twitter")
+
+    git.click(function () {
+
+        $(location).attr('href', 'https://github.com/')
+
+    })
+
+    face.click(function () {
+
+        $(location).attr('href', 'https://facebook.com/')
+
+    })
+
+    link.click(function () {
+
+        $(location).attr('href', 'https://linkedin.com/')
+
+    })
+
+    twit.click(function () {
+
+        $(location).attr('href', 'https://twitter.com/')
+
+    })
+
 
 })
-
-face.click(function () {
-
-    $(location).attr('href', 'https://facebook.com/')
-
-})
-
-link.click(function () {
-
-    $(location).attr('href', 'https://linkedin.com/')
-
-})
-
-twit.click(function () {
-
-    $(location).attr('href', 'https://twitter.com/')
-
-})
-
-
-
 
 
 
